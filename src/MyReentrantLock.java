7baedb4a2a99d35ba2a41457440aa65755ae39e8
adi <adihat3554@gmail.com>
2022-06-21 13:00:35 +0300

    ....

--- src/MyReentrantLock.java
+++ src/MyReentrantLock.java
@@ -11,18 +11,29 @@ public class MyReentrantLock implements Lock{
         this.counterLock=0;
     }
     @Override
-    public void acquire() {
+    public void acquire() {/*
         try {
             while (Thread.currentThread() != this.lockedThread || !this.tryAcquire()) {
                 this.wait();
             }
         }
-        catch (Exception e){}
+        catch (Exception e){}*/try {
+        while(isLocked.get() && Thread.currentThread() != this.lockedThread){
+            this.wait();
+        }
+        isLocked.set(true);
+        this.lockedThread = Thread.currentThread();
+        this.counterLock++;
+
+    } catch (InterruptedException e){
+    }
         }
 
     @Override
     public boolean tryAcquire() {
     boolean locking =isLocked.compareAndSet(false,true);
+    if (lockedThread==Thread.currentThread())
+        locking=true;
     if (locking) {
         this.lockedThread = Thread.currentThread();
         this.counterLock++;
@@ -31,13 +42,20 @@ public class MyReentrantLock implements Lock{
     }
 
     @Override
-    public void release() throws IllegalReleaseAttempt{
-        if (this.lockedThread==Thread.currentThread()&&isLocked.compareAndSet(true,counterLock!=1)) {
-            counterLock--;
+    public void release() throws IllegalReleaseAttempt{/*
+        if (this.lockedThread==Thread.currentThread()&&isLocked.compareAndSet(true,false)) {
+            //counterLock=0;
+            this.lockedThread=null;
             this.notify();
             return;
         }
-        //throw new IllegalReleaseAttempt();
+       // throw new IllegalReleaseAttempt();*/
+        if(Thread.currentThread() == this.lockedThread){
+            this.counterLock--;
+        }
+        if(this.counterLock == 0 && isLocked.compareAndSet(true,false)){
+            this.notify();
+        }
     }
 
     @Override
@@ -46,7 +64,7 @@ public class MyReentrantLock implements Lock{
             this.release();
         }
         catch (IllegalReleaseAttempt e){
-           // throw e;
+            //throw e;
         }
     }
 }
